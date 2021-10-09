b662d2a317e01343e0ba0b70e6b48c94d0b44cba
Valexr <valexr@gmail.com>
2021-10-09 15:09:58 +0300

    remove defaults

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,5 @@
 const sveltePreprocess = require("svelte-preprocess");
 
-const defaults = {
-	script: "typescript",
-};
-
 module.exports = {
-	preprocess: sveltePreprocess({ defaults })
+	preprocess: sveltePreprocess()
 }
