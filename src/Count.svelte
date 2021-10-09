b5902dd43cdc8cb7951478bcbd507c8d615649d9
Valexr <valexr@gmail.com>
2021-10-09 15:05:34 +0300

    first commit

---
+++ src/Count.svelte
@@ -0,0 +1,26 @@
+<button id="decr" on:click={decrement}>--</button>
+<!-- svelte-ignore missing-declaration -->
+<input value={count} />
+<button id="incr" on:click={increment}>++</button>
+
+<script lang="ts">
+    type CountResult = number;
+    export let count: CountResult = 5;
+
+    function increment(): void {
+        count++;
+    }
+
+    function decrement(): void {
+        count--;
+    }
+</script>
+
+<style lang="scss">
+    button {
+        color: red;
+        // b {
+        //     color: blue;
+        // }
+    }
+</style>
